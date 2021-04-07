6b5cc996c532fe5fccce19336c1e97a94c010793
Dari <72017271+Pluzhnikovadari@users.noreply.github.com>
2021-04-07 23:14:51 +0300

    Add files via upload

--- interact.py
+++ interact.py
@@ -3,115 +3,118 @@ import time
 import os
 import sys
 
-def run_command(cmd,output="print",exit_on_error=False):
-    p = subprocess.Popen(cmd, stdout=subprocess.PIPE,stderr=subprocess.PIPE,shell=True,
-        universal_newlines=True)
+
+def run_command(cmd, output="print", exit_on_error=False):
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE,
+                         stderr=subprocess.PIPE, shell=True,
+                         universal_newlines=True)
     o, e = p.communicate()
     if p.returncode != 0:
-        if output == "print" : print("%s%s" % (o, e))
+        if output == "print":
+            print("%s%s" % (o, e))
         if exit_on_error:
             print("Error. Exit script")
             quit()
-        if output == "return": return("%s%s" % (o, e))
+        if output == "return":
+            return("%s%s" % (o, e))
     else:
-        if output == "return": return("%s%s" % (o, e))
+        if output == "return":
+            return("%s%s" % (o, e))
         print("%s%s" % (o, e))
+
 
 def to_string(ip_path):
     ip_path = ip_path.split('.')
-    res = "1" + "0" * (3 - len(ip_path[0])) + ip_path[0] + "0" * (3 - len(ip_path[1])) + ip_path[1] + "0" * (3 - len(ip_path[2])) + ip_path[2]
-    ip_path = ip_path[-1].split('/')
-    res += "0" * (3 - len(ip_path[0])) + ip_path[0] + ip_path[-1]
+    res = "1" + "0" * (3 - len(ip_path[0])) + ip_path[0] + \
+          "0" * (3 - len(ip_path[1])) + ip_path[1] + \
+          "0" * (3 - len(ip_path[2])) + ip_path[2]
+    if "/" in ip_path:
+        ip_path = ip_path[-1].split('/')
+        res += "0" * (3 - len(ip_path[0])) + ip_path[0] + ip_path[-1]
+    else:
+        res += "0" * (3 - len(ip_path[3])) + ip_path[3] + '24'
     return res
 
 
-our_ip = open('our_ip.txt', 'r')
-cmd = "sudo cat /etc/quagga/bgpd.conf >> our_ip.txt"
-run_command(cmd, "print")
-our_way = our_ip.readlines()[4:6]
-our_way = [our_way[0].split(' ')[-1][:-1], to_string(our_way[1].split(' ')[-1][:-1])]
-run_command(cmd, "print")
-tablewrite = open('bgptable.txt', 'w')
-table = open('bgptable.txt', 'r')
-update = open('bgptable_updates.txt', 'r')
-updatewrite = open('bgptable_updates.txt', 'w')
-event_get = open("substrate-api-client_1/events.txt", "r")
-event = open("substrate-api-client_1/events.txt", "w")
-
-send_data_write = open('substrate-api-client_1/data.txt', 'w')
-
-event.truncate()
-tablewrite.truncate()
-updatewrite.truncate()
-
-
-cmd = "sudo vtysh -c 'show ip bgp' >> bgptable.txt"
-run_command(cmd, "print")
-
-'''
-
-while True:
-    #работа с событиями и добавление пути в quagga
-
-    ev = event_get.readlines()
-    if len(ev) == 3:
-        with open("substrate-api-client_1/events.txt", "w"):
-            pass
-        for i in range(3):
-            ev[i] = ev[i].split(", ")[-1][:-2]
-        print(our_way[0], ev[1])
-        print(ev)
-        if our_way[0] == ev[1]:
+def bob_func():
+    with open('our_ip.txt', 'w'):
+        pass
+    cmd = "sudo cat /etc/quagga/bgpd.conf >> our_ip.txt"
+    with open('our_ip.txt', 'r') as our_ip:
+        run_command(cmd, "print")
+        our_way = our_ip.readlines()[4:6]
+    our_way = [our_way[0].split(' ')[-1][:-1],
+               to_string(our_way[1].split(' ')[-1][:-1])]
+    run_command(cmd, "print")
+    event_get = open("substrate-api-client/events.txt", "r")
+    while True:
+        # работа с событиями и добавление пути в quagga.
+        time.sleep(1)
+        ev = event_get.readlines()
+        if ev:
+            ev = ev[-1].split(", ")[-1][:-2]
             if os.geteuid() == 0:
-                print("We're root!")
-                f = open('/etc/quagga/bgpd.conf', "r+")
-                line = f.readlines()
-                add = str(int(ev[0][1:4])) + "." + str(int(ev[0][4:7])) + "." + str(int(ev[0][7:10])) + "." + str(int(ev[0][10:13])) + '/' + str(int(ev[0][13:]))
-                for i in range(len(line)):
-                    print(line[i])
-                    if ('neighbor' in line[i]) or ('network' in line[i]) or (i == len(line) - 1):
-                        line = line[:i] + [' network ' + add + '\n'] + line[i:]
-                        print(line)
-                        f = open('/etc/quagga/bgpd.conf', "w+")
+                with open('/etc/quagga/bgpd.conf', "r+") as f:
+                    line = f.readlines()
+                    add = str(int(ev[1:4])) + "." + str(int(ev[4:7])) + \
+                        "." + str(int(ev[7:10])) + "." + \
+                        str(int(ev[10:13])) + '/' + str(int(ev[13:]))
+                    for i in range(len(line)):
+                        """ ip prefix-list PREFIX-LIST-FROM-BGP1
+                        seq 2 permit 169.254.253.0/24."""
+                        if ('route-map MAP-FROM-BGP1 permit' in line[i]):
+                            line = line[:i] + [' ip prefix-list \
+                                PREFIX-LIST-FROM-BGP1 seq \
+                                1 permit ' + add + '\n'] + ['!\n'] + line[i:]
+                            break
+                    with open('/etc/quagga/bgpd.conf', "w") as f:
                         for el in line:
                             f.write(el)
             else:
                 subprocess.call(['sudo', 'python3', *sys.argv])
                 sys.exit()
-            
-        print(ev)
-    
-    updatewrite = open('bgptable_updates.txt', 'w')
 
-    
-    cmd = "sudo vtysh -c 'show ip bgp' >> bgptable_updates.txt"
+
+def alice_func():
+    cmd = "sudo vtysh -c 'show ip bgp' >> bgptable.txt"
     run_command(cmd, "print")
-    
-    tablefile = table.readlines()[6:-2]
-    updatefile = update.readlines()[6:-2]
-    t = set(tablefile)
-    u = set(updatefile)
-    t.difference_update(u)
-    for elem in t:
-        ip_path = elem.split()[1]
-        ip_path = to_string(ip_path)
-        print('send', ip_path) 
-        print('send', our_way)
+    while True:
+        with open('bgptable_updates.txt', 'w'):
+            pass
 
-        with open('substrate-api-client_1/data.txt', 'w') as send_data_write:
-            send_data_write.write(str(ip_path))
-        cmd = "cd substrate-api-client_1/ && cargo +nightly-2020-10-01 run --example example_generic_extrinsic"
-        run_command(cmd, "print")
+        cmd = "sudo vtysh -c 'show ip bgp' >> bgptable_updates.txt"
+        run_command(cmd, "skip")
 
-        with open('substrate-api-client_1/data.txt', 'w') as send_data_write:
-            send_data_write.write(our_way[0]) #send our number of router
-        run_command(cmd, "print")
+        with open('bgptable_updates.txt', 'r') as update:
+            with open('bgptable.txt', 'r') as table:
+                tablefile = table.readlines()[6:-2]
+                updatefile = update.readlines()[6:-2]
 
-        with open('substrate-api-client_1/data.txt', 'w') as send_data_write:
-            send_data_write.write(our_way[1]) #send our ip number
-        run_command(cmd, "print")
-    
-    with  open('bgptable.txt', 'w') as tablewrite:
-        data = update.read()
-        tablewrite.write(data)
-'''
+                t = set(tablefile)
+                u = set(updatefile)
+                t.difference_update(u)
+                for elem in t:
+                    ip_path = elem.split()[1]
+                    ip_path = to_string(ip_path)
+
+                    with open('substrate-api-client_1/data.txt',
+                              'w') as send_data_write:
+                        send_data_write.write(ip_path)
+                    cmd = "cd substrate-api-client_1/ && \
+                    cargo +nightly-2020-10-01 run --example \
+                    example_generic_extrinsic"
+                    run_command(cmd, "print")
+            with open('bgptable.txt', 'w') as tablewrite:
+                update.seek(0)
+                data = update.read()
+                tablewrite.write(data)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        print("Add description for mode")
+    else:
+        if sys.argv[1] == "bob":
+            bob_func()
+        elif sys.argv[1] == "alice":
+            alice_func()
